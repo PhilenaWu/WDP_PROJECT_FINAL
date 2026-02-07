d32ff17b64e3723a8d643be6dfc6d9fe5b5ab35f
projectedjz <242942m@mymail.nyp.edu.sg>
2026-02-07 21:40:48 +0800

    database

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-change-this'
     
     # MySQL Database
-    MYSQL_HOST = os.environ.get('MYSQL_HOST') or 'localhost'
+    MYSQL_HOST = os.environ.get('MYSQL_HOST') or 'mainline.proxy.rlwy.net'
     MYSQL_USER = os.environ.get('MYSQL_USER') or 'root'
-    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD') or 'your_password'
-    MYSQL_DATABASE = os.environ.get('MYSQL_DATABASE') or 'storyconnect_db'
-    MYSQL_PORT = int(os.environ.get('MYSQL_PORT', 3306))
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD') or ''
+    MYSQL_DATABASE = os.environ.get('MYSQL_DATABASE') or 'railway'
+    MYSQL_PORT = int(os.environ.get('MYSQL_PORT', 27748))
     
     # Session
     SESSION_TYPE = 'filesystem'
