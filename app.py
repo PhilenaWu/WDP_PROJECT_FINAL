210575f0e72918f1e41323fb777a7472e93c64b9
mahdiyaa <mahd.maz.08@gmail.com>
2026-02-16 10:44:01 +0800

    ,,,

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from eventlet import monkey_patch
+monkey_patch()  # Patch standard library for compatibility with eventlet
+
 from dotenv import load_dotenv
 load_dotenv()  # Load environment variables from .env file
 
